714e5d3527eedf2d4724ffad2b9045eb764d398c
AFreul kaium Joy <afreuljoy@gmail.com>
2022-04-24 03:25:46 +0600

    bubble sort comming

---
+++ Algo/Buble_Sort.js
@@ -0,0 +1,37 @@
+const numbers = [14,10,27,33,35];
+
+// 14,10,27,33,35
+// 14,10,27,33,35
+// 14,10,27,33,35
+// 14,10,27,33,35
+
+
+
+function bubbleSortAscending(array){
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if(array[j] > array[j+1]){
+                const temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+function bubbleSortDesc(array){
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if(array[j] < array[j+1]){
+                const temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+const sorted = bubbleSort(numbers);
+console.log(sorted);
