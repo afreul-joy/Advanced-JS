7f4740ab823a7a325340ed30c6dbe54a4a0d1c31
AFreul kaium Joy <afreuljoy@gmail.com>
2022-04-24 16:38:42 +0600

    bubble sort done

--- Algo/Buble_Sort.js
+++ Algo/Buble_Sort.js
@@ -1,17 +1,26 @@
-const numbers = [14,10,27,33,35];
+const numbers = [45,87,90,56,12,11,6,52];
 
-// 14,10,27,33,35
-// 14,10,27,33,35
-// 14,10,27,33,35
-// 14,10,27,33,35
+//----------আগের সংখ্যার সাথে পরের সংখ্যার কম্পেয়ার করব, যদি আগের সংখ্যা বড় হয় তবে swapp হবে ----
 
+// 45,87,90,56,12,11,6,52  
 
+// 45,87,56,12,11,6,52,90
 
-function bubbleSortAscending(array){
-    for (let i = 0; i < array.length; i++) {
+// 45,56,12,11,6,52,87,90
+
+// 45,12,11,6,52,56,87,90
+
+// 12,11,6,45,52,56,87,90
+
+// 11,6,12,45,52,56,87,90
+
+// 6,11.12,45,52,56,87,90
+
+function bubbleSortAscending(array){  
+    for (let i = 0; i < array.length; i++) {  // লুপ ২টি চালাতে হবে কারন বাম থেকে ডানে যাবে আবার উপর থেকে নিচে
         for (let j = 0; j < array.length; j++) {
-            if(array[j] > array[j+1]){
-                const temp = array[j];
+            if(array[j] > array[j+1]){            // array[j]= আগের সংখ্যা  array[j+1] = পরের সংখ্যা
+                const temp = array[j];              // swapping
                 array[j] = array[j+1];
                 array[j+1] = temp;
             }
@@ -33,5 +42,5 @@ function bubbleSortDesc(array){
     return array;
 }
 
-const sorted = bubbleSort(numbers);
+const sorted = bubbleSortAscending(numbers);
 console.log(sorted);
